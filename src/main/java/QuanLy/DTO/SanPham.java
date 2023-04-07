a28aea3cbf90c45e5f4b273dfae87f7f2c0d66c0
Wibulization <nhmkhoi123@gmail.com>
2023-04-07 15:40:52 +0700

    Revert "init"
    This reverts commit d492e34452b81770be30fc7d607caf3b4a95c56b.

---
+++ src/main/java/QuanLy/DTO/SanPham.java
@@ -0,0 +1,99 @@
+
+package QuanLy.DTO;
+
+public class SanPham {
+    private int maSP;
+    private String tenSP;
+    private int maLoai;
+    private int soLuong;
+    private String donViTinh;
+    private String hinhAnh;
+    private int donGia;
+
+    public SanPham() {
+
+    }
+
+    public SanPham(int maSP, String tenSP, int maLoai, int soLuong, String donViTinh, String hinhAnh, int donGia) {
+        this.maSP = maSP;
+        this.tenSP = tenSP;
+        this.maLoai = maLoai;
+        this.soLuong = soLuong;
+        this.donViTinh = donViTinh;
+        this.hinhAnh = hinhAnh;
+        this.donGia = donGia;
+    }
+
+    public int getMaSP() {
+        return maSP;
+    }
+
+    public void setMaSP(int maSP) {
+        this.maSP = maSP;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public int getMaLoai() {
+        return maLoai;
+    }
+
+    public void setMaLoai(int maLoai) {
+        this.maLoai = maLoai;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public String getDonViTinh() {
+        return donViTinh;
+    }
+
+    public void setDonViTinh(String donViTinh) {
+        this.donViTinh = donViTinh;
+    }
+
+    public String getHinhAnh() {
+        return hinhAnh;
+    }
+
+    public void setHinhAnh(String hinhAnh) {
+        this.hinhAnh = hinhAnh;
+    }
+
+    public int getDonGia() {
+        return donGia;
+    }
+
+    public void setDonGia(int donGia) {
+        this.donGia = donGia;
+    }
+
+    /*public Object[] toArray()
+    {
+        return new Object[] {maSP, tenSP, soLuong,donViTinh, hinhAnh, donGia};
+    }
+    */
+    
+    @Override
+    public String toString() {
+        return
+                "maSP=" + maSP +
+                ", tenSP='" + tenSP + '\'' +
+                ", soLuong=" + soLuong +
+                ", donViTinh='" + donViTinh + '\'' +
+                ", hinhAnh='" + hinhAnh + '\'' +
+                ", donGia=" + donGia ;
+    }
+}
